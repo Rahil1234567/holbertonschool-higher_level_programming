bf7ed77cd2a4b66f2b20d8bd579677dab0d2076b
Rahil1234567 <rahilliyev2005@gmail.com>
2026-04-11 19:13:40 +0000

    add file

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -4,6 +4,4 @@
 
 class Square:
     """This class is used to define a square shape"""
-    def __init__(self, size):
-        """Initialized the square with the entered size"""
-        self.__size = size
+    pass
